89886603c7a7b29beb34b3147197fb648e7507ec
veeps <vpeng12@gmail.com>
2020-07-25 22:43:06 -0700

    exploring violin plots

--- snapshot.R
+++ snapshot.R
@@ -1,17 +1,24 @@
 library(tidyverse)
 library(janitor)
 library(RColorBrewer)
+library(circlize)
+library(patchwork)
+library(viridis)
+library(networkD3)
 
-df <- read_csv("data/JC-202006-citibike-tripdata.csv")
+df <- read_csv("data/citibike-tripdata.csv")
 
 # clean up column names
-df <- clean_names(df)
+#df <- clean_names(df)
 
 # create an age column
-df$age <- 2020 - df$birth_year
+#df$age <- 2020 - df$birth_year
 
 # write to csv
-write_csv(df, "data/citibike-tripdata.csv", append = FALSE)
+#write_csv(df, "data/citibike-tripdata.csv", append = FALSE)
+
+head(df)
+
 
 # summary stats
 summary(df)
@@ -36,8 +43,11 @@ group_by(start_station_name) %>%
 # check unique values for usertype
 unique(df$usertype)
 
+
 # plot histogram
-ggplot(head(summary, 10), aes(y=avg_duration, x=start_station_name , fill = ifelse(avg_duration > 3000, "Over an hour", "Less than an hour"))) + 
+summary %>%
+  head(10) %>%
+  ggplot(aes(y=avg_duration, x=start_station_name , fill = ifelse(avg_duration > mean(avg_duration), "Over an hour", "Less than an hour"))) + 
   geom_bar(stat="identity", show.legend = F) + scale_fill_manual(values = c('#828282', '#5eff8a')) +
   ggtitle("Average Trip Duration by Start Station", ) + theme(plot.title = element_text(hjust = 0.5)) + 
   ylab("Trip Duration (Seconds)") +
@@ -46,7 +56,7 @@ ggplot(head(summary, 10), aes(y=avg_duration, x=start_station_name , fill = ifel
   #scale_fill_brewer(palette="Blues")
 
 # plot age distribution
-ggplot(df, aes(x = age)) + geom_density()
+ggplot(df, aes(x = age, fill = usertype, alpha = 0.5)) + geom_density()
 
 # plot time distribution
 ggplot(df, aes(y = age, x = usertype, fill = usertype)) + geom_violin()
@@ -57,3 +67,41 @@ df2 <- df %>%
 
 # plot time distribution
 ggplot(df2, aes(y = tripduration, x = usertype, fill = usertype)) + geom_violin()
+
+
+## Create Sankey Graph
+df_sankey <- df %>%
+  group_by(start_station_name, end_station_name) %>%
+  summarise(
+    rides = n()
+  ) %>%
+  filter(start_station_name != end_station_name) %>%
+  arrange(-rides) %>%
+  head(10) %>%
+  as.data.frame()
+
+df_sankey$end_station_name<- paste(df_sankey$end_station_name, " ")
+
+
+# From these flows we need to create a node data frame: it lists every entities involved in the flow
+nodes <- data.frame(name=c(as.character(df_sankey$start_station_name), as.character(df_sankey$end_station_name)) %>% unique())
+
+
+# With networkD3, connection must be provided using id, not using real name like in the links dataframe.. So we need to reformat it.
+df_sankey$start_id=match(df_sankey$start_station_name, nodes$name)-1 
+df_sankey$end_id=match(df_sankey$end_station_name, nodes$name)-1
+
+
+
+# prepare colour scale
+ColourScal ='d3.scaleOrdinal() .range(["#FDE725FF","#B4DE2CFF","#6DCD59FF","#35B779FF","#1F9E89FF","#26828EFF","#31688EFF","#3E4A89FF","#482878FF","#440154FF"])'
+
+
+# Make the Network
+sankeyNetwork(Links = df_sankey, Nodes = nodes,
+              Source = "start_id", Target = "end_id",
+              Value = "rides", NodeID = "name", 
+              sinksRight=FALSE,  nodeWidth=40, fontSize=13, nodePadding=10, )
+
+
+
