1ee126e480fdaa884b2f67bfbbec35b5e8ffaf35
veeps <vpeng12@gmail.com>
2020-07-28 20:46:53 -0700

    added reactive options

--- app.R
+++ app.R
@@ -19,18 +19,28 @@ ui <- fluidPage(
         ),
 
         
-        # new row for summary chart
+        # new row for summary bar chart
         fluidRow(style="padding-top:40px; padding-left: 40px",
-          # plot chart
-          column(3, selectInput(inputId="yaxis", #references the input to server
-                                label = h3("Select Variable"), # text that appears on UI
-                                choices=c("Avg Duration", "Avg Age", "Total Rides"))),
-          column(9,plotOutput("bar_plot"))
-        )
+                # create side panel with dropdown menu
+                column(3, selectInput(inputId="bar_yaxis", #references the input to server
+                                      label = h3("Select Variable"), # text that appears on UI
+                                      choices=c("Avg Duration", "Avg Age", "Total Rides"))),
+                # plot bar chart
+                column(9,plotOutput("bar_plot"))
+        ),
         
-       #fluidRow(
-        # plotly::plotlyOutput("scatter")
-        #)
+        
+        # new row for scatter plot
+        fluidRow(style="padding-left:40px",
+          column(3,
+                 # create radio buttons
+                 div(radioButtons(inputId="xaxis", # references the input from server
+                                  label = "X Variable", # Label that appears on UI
+                                  choices=c("Age", "Gender")# list of input options
+                 ))),
+          # plot chart
+          column(9,plotly::plotlyOutput("scatter"))
+          )
 )
 
 
@@ -54,13 +64,18 @@ server <- function(input, output) {
 
   # render data table
   output$summary <- renderDT(summary, options=list( info = FALSE, paging = TRUE, searching = TRUE))
+  
+  # create reactive input for bar plot
+  bar_y <- reactive({
+    if ("Avg Duration" %in% input$bar_yaxis) return(summary$avg_duration)
+    if ("Avg Age" %in% input$bar_yaxis) return(summary$avg_age)
+    if ("Total Rides" %in% input$bar_yaxis) return (summary$total_rides)
+  })
 
   
   # render a barplot for summary table
   output$bar_plot <- renderPlot({
-    
-  # plot histogram
-  ggplot(summary, aes(y=total_rides, x=start_station_name , fill = ifelse(total_rides > mean(total_rides), T, F))) + 
+  ggplot(summary, aes(y=bar_y(), x=start_station_name , fill = ifelse(bar_y() > mean(bar_y()), T, F))) + 
     geom_bar(stat="identity", show.legend = F) + scale_fill_manual(values = c('#828282', '#5eff8a')) +
     ggtitle("Average Trip Duration by Start Station", ) + theme(plot.title = element_text(hjust = 0.5)) + 
     ylab("Total Rides") +
@@ -68,17 +83,20 @@ server <- function(input, output) {
     scale_x_discrete(labels=function(x){gsub(" ", "\n", summary$start_station_name)})
   })
   
-  # render scatter plot
-  output$scatter <- renderPlotly({
-    plot_ly(data = df, x = ~age, y = ~tripduration,
-            maker=list(size=5),
-            text = ~paste("Age:", age, "Trip Duration:", tripduration/60, "min"))
+  # create reactive input for scatter plot x axis
+  scatter_x <- reactive({
+    if ("Age" %in% input$xaxis) return(df$age)
+    if ("Gender" %in% input$bar_yaxis) return(dg$age)
   })
   
-  # plot  distribution
-  output$distribution <- renderPlot({
-    ggplot(df, aes(y = age, x = gender, fill = gender, alpha = 0.5)) + geom_violin(show.legend = F)
+  # render scatter plot
+  output$scatter <- renderPlotly({
+  plot_ly(data = df, x = ~scatter_x(), y = ~tripduration,
+      maker=list(size=5),
+      text = ~paste("Age:", age, "Trip Duration:", tripduration/60, "min"))
   })
+  
+
 
   
   
