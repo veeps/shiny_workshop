95f971bbb4bfaa8713c82897b64ea8769cea2c5e
veeps <vpeng12@gmail.com>
2020-08-05 23:33:36 -0700

    update examples

--- 02_project/app.R
+++ 02_project/app.R
@@ -6,17 +6,7 @@ library(RColorBrewer)
 
 ui <- fluidPage(
   
-  # # new row for summary bar chart
-  # fluidRow(style="padding-top:40px; padding-left: 40px",
-  #          # create side panel with dropdown menu
-  #          column(3, selectInput(inputId="bar_yaxis", #references the input to server
-  #                                label = h3("Select Variable"), # text that appears on UI
-  #                                choices=c("Avg Duration", "Avg Age", "Total Rides"))),
-  #          # plot bar chart
-  #          column(9,plotOutput("bar_plot"))
-  # )
-  
-  fluidRow(plotOutput("bar_plot"))
+  fluidRow()
 )
 
 server <- function(input, output, session) {
@@ -37,19 +27,19 @@ server <- function(input, output, session) {
   
   
   # render data table output
-  output$summary <- renderDT(summary, options=list( info = FALSE, paging = F, searching = F))
+  output$summary_dt <- renderDT()
   
   
   # render barchart
-  output$bar_plot <- renderPlot({
-    ggplot(summary, aes(x=start_station_name, y = total_rides, fill = start_station_name)) + 
-      geom_bar(stat="identity", show.legend=F) +
-      ggtitle("Total Rides by Station") + 
-      theme(plot.title = element_text(hjust = 0.5)) + 
-      scale_x_discrete(labels=function(x){gsub(" ", "\n", summary$start_station_name)}) +
-      ylab("Total Rides") +
-      xlab("Start Station")
-    })
+  #output$bar_plot <- renderPlot({})
+  
+  # create reactive input for bar plot
+  #bar_y <- reactive({})
+  
+  # exercise - create reactive input for chart title
+  
+  # create scatterplot
+  # output$scatter <- renderPlotly({})
   
 }
 
--- 02_project/eda.R
+++ 02_project/eda.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-
+library(plotly)
 
 # read in data
 df <- readr::read_csv("./data/citibike-tripdata.csv")
@@ -7,22 +7,26 @@ df <- readr::read_csv("./data/citibike-tripdata.csv")
 
 
 # create a summary table
-# summary <- df %>%
-#   group_by(start_station_name) %>%
-#   summarise(
-#     total_rides = n(),
-#     avg_duration= as.integer(mean(tripduration)),
-#     avg_age = as.integer(mean(age))
-#   ) %>%
-#   arrange(-total_rides) %>%
-#   head(10)
+summary <- df %>%
+  group_by(start_station_name) %>%
+  summarise(
+    total_rides = n(),
+    avg_duration= as.integer(mean(tripduration)),
+    avg_age = as.integer(mean(age))
+  ) %>%
+  arrange(-total_rides) %>%
+  head(10)
 
 
 # create a bar plot
+ggplot() +
+  geom_bar(stat="identity", show.legend=F) +
+  ggtitle("Total Rides by Station") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  scale_x_discrete(labels=function(x){gsub(" ", "\n", summary$start_station_name)})
 
-# ggplot() + 
-#   geom_bar(stat="identity", show.legend=F) +
-#   ggtitle("Total Rides by Station") + 
-#   theme(plot.title = element_text(hjust = 0.5)) + 
-#   scale_x_discrete(labels=function(x){gsub(" ", "\n", summary$start_station_name)})
-# 
+# create a scatter plot the ggplot way
+ggplot() + geom_point()
+
+# create scatter plot with plotly
+plot_ly()
