e029e911399dd728c93ab255defa038235d6761f
veeps <vpeng12@gmail.com>
2020-08-09 18:15:26 -0700

    going to do summary code live

--- 02_project/eda.R
+++ 02_project/eda.R
@@ -7,15 +7,6 @@ df <- readr::read_csv("./data/citibike-tripdata.csv")
 
 
 # create a summary table
-summary <- df %>%
-  group_by(start_station_name) %>%
-  summarise(
-    total_rides = n(),
-    avg_duration= as.integer(mean(tripduration)),
-    avg_age = as.integer(mean(age))
-  ) %>%
-  arrange(-total_rides) %>%
-  head(10)
 
 
 # create a bar plot
