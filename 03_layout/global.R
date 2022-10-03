132a85a45f9e88ffc152bbc00872323853dc3539
Peng <vpeng@NYMC02FG443MD6R.local>
2022-10-03 16:22:15 -0400

    prepping files for live coding

--- 03_layout/global.R
+++
@@ -1,9 +0,0 @@
-library(shiny)
-library(readr)
-library(dplyr)
-
-df <- readr::read_csv("https://docs.google.com/spreadsheets/d/1rOTd_fdh1kOXgya7XBP7TIR2ETkYfZx0hBz4bERLxss/export?format=csv")
-
-
-# if I want to do more data cleaning, I can do it here only once.
-
--- 03_layout/layout.R
+++ 03_layout/layout.R
@@ -1,7 +1,15 @@
 library(shiny)
 
+ui <- fluidPage(
 
+)
 
+server <- function(input, output, session) {
+  
+  
+  
+  
+}
 
 
 shinyApp(ui, server)
